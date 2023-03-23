2e5dc4a54fdcb24c542c5c8f90fad57f1b2a0d6c
unknown <ana.camargo5@estudante.ifms.edu.br>
2023-03-23 18:16:11 -0400

    projeto hero

---
+++ my-app/src/app/components/hero/hero.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  numeros = [ 5, 10, 15, 20, 25 ]
+  heroes = ['capitao america', 'homem de ferro', 'homem formiga']
+
+}
